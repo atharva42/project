656ea17e579ea39d412f124565432ae08d06b0fc
Atharva Pande <rmint8876@gmail.com>
2026-06-13 13:46:03 +0530

    resolving production bug: 2

--- QueryMind/frontend/src/config.js
+++ QueryMind/frontend/src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  API_BASE_URL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8000"
+  API_BASE_URL: import.meta.env.VITE_API_BASE_URL || "https://querymind-smg2.onrender.com"
 };
 
 export default config;
